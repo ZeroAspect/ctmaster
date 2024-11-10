ea47dc8cb40b95906d64165bbc9d0826c3ad2ce9
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-11-10 12:01:15 -0300

    creating authorization using jwt

---
+++ auth/auth-jwt.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken")
+
+// const user = {
+//   id: 1,
+//   name: 'John Doe',
+//   email: 'johndoe@example.com',
+//   permissions: ['admin']
+// };
+
+module.exports = jwt
