bc5142efd392400c89be310ee1259fa5d67f10b0
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-11-09 22:51:08 -0300

    feat: verify if IP return

--- routes.js
+++ routes.js
@@ -1,6 +1,16 @@
 const express = require("express")
 const app = require("./config/config.js")
+const hbs = require("express-handlebars")
+const path = require("path")
+const { GetIPFunction } = require("./funcs/api/ip.js")
+
+
+app.engine("handlebars", hbs.engine())
+app.set("view engine", "handlebars")
+app.set("views", path.join(__dirname + "/views"))
 
 app.get('/', async(req, res)=>{
-  res.send('Hello World!')
+  const ip = await GetIPFunction()
+  console.log(ip)
+  res.render("home")
 })
