3ac624bbfbdd1ad8ce70b0fee003433394718f13
ZeroAspect <joseiraildescipriano123@gmail.com>
2024-11-10 22:46:54 -0300

    fix(error): fix error in passport module'

--- routes.js
+++ routes.js
@@ -3,13 +3,10 @@ const app = require("./config/config.js")
 const hbs = require("express-handlebars")
 const path = require("path")
 const { GetIPFunction } = require("./funcs/api/ip.js")
-const passport = require('passport');
 const checkUser = require("./auth/AuthUser.js")
 const User = require("./models/User.js")
 const db = require("./connections/sequelize.js")
 const MySql = require("./db/database.js")
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 app.engine("handlebars", hbs.engine())
 app.set("view engine", "handlebars")
